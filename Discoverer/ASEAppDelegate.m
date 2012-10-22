aca69d4e26ac86d95eea7004b2f54773ac949d68
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-22 11:21:01 -0600

    Search and publish interact service

--- Discoverer/ASEAppDelegate.m
+++ Discoverer/ASEAppDelegate.m
@@ -1,18 +1,29 @@
-//
-//  ASEAppDelegate.m
-//  Discoverer
-//
-//  Created by Arlo Louis O'Keeffe on 2012-10-22.
-//  Copyright (c) 2012 ASE Group. All rights reserved.
-//
-
 #import "ASEAppDelegate.h"
+
+#import <ServiceDiscovery/ServiceDiscovery.h>
+#import <CocoaLumberjack/DDLog.h>
+#import <CocoaLumberjack/DDTTYLogger.h>
+
+@interface ASEAppDelegate ()
+
+@property (nonatomic, strong) SDServiceDiscovery * serviceDiscovery;
+
+@end
 
 @implementation ASEAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+
+    self.serviceDiscovery = [SDServiceDiscovery new];
+    [self.serviceDiscovery searchForServicesOfType:@"_intairact._tcp"];
+    [self.serviceDiscovery publishServiceOfType:@"_intairact._tcp" onPort:80];
+}
+
+-(BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+{
+    return YES;
 }
 
 @end
