bed44e8fab2797ff15d83b58ea0ad5341aa6c17b
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-22 17:12:22 -0600

    Add buttons to control search and publish individually, added ping pong button that starts and stops publishing periodically

--- Discoverer/ASEAppDelegate.m
+++ Discoverer/ASEAppDelegate.m
@@ -7,8 +7,12 @@
 @interface ASEAppDelegate ()
 
 @property (nonatomic, strong) SDServiceDiscovery * serviceDiscovery;
-@property (weak) IBOutlet NSButton *startStopButton;
-@property BOOL isRunning;
+@property BOOL isSearching;
+@property BOOL isPublishing;
+
+@property (weak) IBOutlet NSButton *searchToggleButton;
+@property (weak) IBOutlet NSButton *publishToggleButton;
+@property (weak) IBOutlet NSButton *pingPongButton;
 
 @end
 
@@ -21,13 +25,15 @@
     self.serviceDiscovery = [SDServiceDiscovery new];
     [self.serviceDiscovery searchForServicesOfType:@"_intairact._tcp"];
     [self.serviceDiscovery publishServiceOfType:@"_intairact._tcp" onPort:80];
-    self.isRunning = YES;
+    self.isSearching = YES;
+    self.isPublishing = YES;
 }
 
 -(void)applicationWillTerminate:(NSNotification *)notification
 {
     [self.serviceDiscovery stop];
-    self.isRunning = NO;
+    self.isSearching = NO;
+    self.isPublishing = NO;
 }
 
 -(BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
@@ -35,17 +41,51 @@
     return YES;
 }
 
-- (IBAction)startStop:(id)sender {
-    if(self.isRunning) {
-        [self.serviceDiscovery stop];
-        self.isRunning = NO;
-        self.startStopButton.title = @"Start";
+- (IBAction)toggleSearch:(id)sender
+{
+    if(self.isSearching) {
+        [self.serviceDiscovery stopSearching];
+        self.isSearching = NO;
+        self.searchToggleButton.title = @"Start Search";
     } else {
         [self.serviceDiscovery searchForServicesOfType:@"_intairact._tcp"];
+        self.isSearching = YES;
+        self.searchToggleButton.title = @"Stop Search";
+    }    
+}
+
+- (IBAction)togglePublishing:(id)sender
+{
+    if(self.isPublishing) {
+        [self.serviceDiscovery stopPublishing];
+        self.isPublishing = NO;
+        self.publishToggleButton.title = @"Start Publishing";
+    } else {
         [self.serviceDiscovery publishServiceOfType:@"_intairact._tcp" onPort:80];
-        self.isRunning = YES;
-        self.startStopButton.title = @"Stop";
+        self.isPublishing = YES;
+        self.publishToggleButton.title = @"Stop Publishing";
     }
+}
+
+- (IBAction)pingPong:(id)sender {
+    [self.searchToggleButton setEnabled:NO];
+    [self.publishToggleButton setEnabled:NO];
+    [self.pingPongButton setEnabled:NO];
+    
+    __block int i = 0;
+    __block dispatch_block_t block;
+    block = ^{
+        if(i < 10) {
+            [self togglePublishing:nil];
+            i++;
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), block);
+        } else {
+            [self.searchToggleButton setEnabled:YES];
+            [self.publishToggleButton setEnabled:YES];
+            [self.pingPongButton setEnabled:YES];
+        }
+    };
+    dispatch_async(dispatch_get_main_queue(), block);
 }
 
 @end
