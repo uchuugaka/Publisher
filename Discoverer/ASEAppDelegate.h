5958dbda30af75a5d2d491e75a37cf3f00cb689c
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-22 11:03:00 -0600

    Initial import

---
+++ Discoverer/ASEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASEAppDelegate.h
+//  Discoverer
+//
+//  Created by Arlo Louis O'Keeffe on 2012-10-22.
+//  Copyright (c) 2012 ASE Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ASEAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Discoverer/ASEAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ASEAppDelegate.m
+//  Discoverer
+//
+//  Created by Arlo Louis O'Keeffe on 2012-10-22.
+//  Copyright (c) 2012 ASE Group. All rights reserved.
+//
+
+#import "ASEAppDelegate.h"
+
+@implementation ASEAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
